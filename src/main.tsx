70828c82d0313459783b2572cc60f98dd373fb35
Harmanpreet Singh <harmanpunn@gmail.com>
2025-08-11 17:30:17 -0400

    Revert "feat: implement static site generation with vite-react-ssg"

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { ViteReactSSG } from 'vite-react-ssg'
-import { routes } from './App'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 
-export const createRoot = ViteReactSSG(
-  { routes },
-  ({ app, router, routes, isClient, initialState }) => {
-    // App setup that runs on both server and client
-    console.log('ViteReactSSG setup:', { isClient, routesCount: routes.length });
-  }
-);
+createRoot(document.getElementById("root")!).render(<App />);
